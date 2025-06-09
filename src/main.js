f15cf1ac4a2c02c39f217e590c1becb9bf56863d
Arif Imeri <arifimeri.11@gmail.com>
2025-06-09 22:40:31 +0200

    Fixed images

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import '@/assets/img'
 import '@/assets/js/main.js'
-import '@/assets/scss'
-import '@/assets/vendor/bootstrap'
-import '@/assets/vendor/bootstrap-icons'
+import '@/assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
+import '@/assets/vendor/bootstrap-icons/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
 createApp(App).mount('#app')
